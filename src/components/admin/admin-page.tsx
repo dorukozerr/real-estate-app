0001b812a403ac9e52f810bad59e904f4264eec2
Doruk Özer <dorukozer@protonmail.com>
2024-11-02 01:14:44 +0300

    feat(admin): created property dialog
    - added dialog component
    - installed needed dependency
    - initialized property dialog component

--- src/components/admin/admin-page.tsx
+++ src/components/admin/admin-page.tsx
@@ -3,9 +3,11 @@
 import { useState } from "react";
 import Image from "next/image";
 import { ImageKitProvider, IKUpload } from "imagekitio-next";
+import { Plus } from "lucide-react";
 import { createProperty, updateProperty, deleteProperty } from "@/actions";
 import { Property } from "@/types";
 import { imagekitioAuthenticator } from "@/lib/imagekitio-authenticator";
+import { PropertyDialog } from "@/components/admin/property-dialog";
 import {
   Table,
   TableBody,
@@ -14,41 +16,33 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-
-const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
-const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
-
-// <ImageKitProvider
-//   urlEndpoint={urlEndpoint}
-//   publicKey={publicKey}
-//   authenticator={imagekitioAuthenticator}
-// >
-//   <div>
-//     <IKUpload
-//       onError={(err) => console.error("Image Upload Error =>", err)}
-//       onSuccess={(res) => {
-//         console.log("Image Upload Success =>", res);
-//         setProperty((prevState) => ({
-//           ...prevState,
-//           imageUrls: [...(prevState.imageUrls as Array<string>), res.url],
-//         }));
-//       }}
-//     />
-//   </div>
-// </ImageKitProvider>;
-
-const initialPropertyData = {
-  title: "",
-  price: 0,
-  location: "",
-  imageUrls: [],
-  description: "",
-  isFeatured: false,
-  isForRent: false,
-};
+import { Button } from "@/components/ui/button";
 
 export const AdminPage = ({ properties }: { properties: Property[] }) => {
+  const [dialogState, setDialogState] = useState({
+    mode: "",
+    open: false,
+    property: null,
+  });
+
   console.log("properties =>", properties);
 
-  return <div className="h-full w-full border-4 border-red-500"></div>;
+  return (
+    <>
+      <div className="h-full w-full">
+        <div className="flex w-full items-center justify-end">
+          <Button variant="outline" size="icon">
+            <Plus className="h-[1.2rem] w-[1.2rem]" />
+          </Button>
+        </div>
+      </div>
+      <PropertyDialog
+        open={dialogState.open}
+        onOpenChange={(flag) =>
+          setDialogState((prevState) => ({ ...prevState, open: flag }))
+        }
+        property={dialogState.property}
+      />
+    </>
+  );
 };
---
+++ src/components/admin/property-dialog.tsx
@@ -0,0 +1,66 @@
+import { Property } from "@/types";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+
+// <ImageKitProvider
+//   urlEndpoint={urlEndpoint}
+//   publicKey={publicKey}
+//   authenticator={imagekitioAuthenticator}
+// >
+//   <div>
+//     <IKUpload
+//       onError={(err) => console.error("Image Upload Error =>", err)}
+//       onSuccess={(res) => {
+//         console.log("Image Upload Success =>", res);
+//         setProperty((prevState) => ({
+//           ...prevState,
+//           imageUrls: [...(prevState.imageUrls as Array<string>), res.url],
+//         }));
+//       }}
+//     />
+//   </div>
+// </ImageKitProvider>;
+
+const initialPropertyData = {
+  title: "",
+  price: 0,
+  location: "",
+  imageUrls: [],
+  description: "",
+  isFeatured: false,
+  isForRent: false,
+};
+
+export const PropertyDialog = ({
+  open,
+  onOpenChange,
+  property,
+}: {
+  open: boolean;
+  onOpenChange: (flag: boolean) => void;
+  property: Property | null;
+}) => {
+  console.log("property =>", property);
+
+  return (
+    <Dialog onOpenChange={onOpenChange} open={open}>
+      <DialogContent className="">
+        <DialogHeader className="">
+          <DialogTitle>Dialog Title</DialogTitle>
+          <DialogDescription>Dialog Description</DialogDescription>
+        </DialogHeader>
+        <div className=""></div>
+        <DialogFooter className="">Dialog Footer</DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
