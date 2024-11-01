d06b107a449c7d3b822e195c0d09998cd3c571dc
Doruk Özer <dorukozer@protonmail.com>
2024-11-01 23:45:18 +0300

    refactor(admin): cleared admin page component
    - moved authenticator to seperate file
    - removed all content from admin page to start from scratch

---
+++ src/lib/imagekitio-authenticator.ts
@@ -0,0 +1,18 @@
+export const imagekitioAuthenticator = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/auth");
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { signature, expire, token };
+  } catch (error) {
+    throw new Error(`Authentication request failed`);
+  }
+};
